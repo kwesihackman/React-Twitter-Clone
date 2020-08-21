875bf481f02514934dd37a5a5c10a9dee8fe0804
Eric Hackman <kwesihackman@gmail.com>
2020-08-21 17:21:24 +0000

    initial project setup

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {}
+
+const Sidebar = (props: Props) => {
+  return (
+    <div>
+      <h1>Sidebar</h1>
+    </div>
+  );
+};
+
+export default Sidebar;
