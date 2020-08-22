99b584d993747aa6165bad1426911395304a7b02
Eric Hackman <kwesihackman@gmail.com>
2020-08-22 10:20:42 +0000

    finished the post ui setup

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./feed.css";
 import Tweetbox from "../tweetbox/Tweetbox";
+import Post from "../post/Post";
 
 interface Props {}
 
@@ -11,6 +12,35 @@ export const Feed = (props: Props) => {
         <h2>Home</h2>
       </div>
       <Tweetbox />
+      <Post
+        avatar="https://api.adorable.io/avatars/400/43e7b55ece917cd7d98428a18871bd97.png"
+        displayName="Eric Hackman"
+        username="@eric_hackman"
+        verified
+        image="https://media.giphy.com/media/duzpaTbCUy9Vu/giphy.gif"
+      />
+      <Post
+        avatar="https://api.adorable.io/avatars/400/43e7b55ece917cd7d98428a18871bd97.png"
+        displayName="Eric Hackman"
+        username="@eric_hackman"
+        verified
+        image="https://media.giphy.com/media/duzpaTbCUy9Vu/giphy.gif"
+      />
+      <Post
+        avatar="https://api.adorable.io/avatars/400/43e7b55ece917cd7d98428a18871bd97.png"
+        displayName="Eric Hackman"
+        username="@eric_hackman"
+        verified
+        image="https://media.giphy.com/media/duzpaTbCUy9Vu/giphy.gif"
+      />
+      <Post
+        avatar="https://api.adorable.io/avatars/400/43e7b55ece917cd7d98428a18871bd97.png"
+        displayName="Eric Hackman"
+        username="@eric_hackman"
+        verified
+        image="https://media.giphy.com/media/duzpaTbCUy9Vu/giphy.gif"
+      />
+      {/* <Post /> */}
       {/* tweetbox */}
       {/* feedlist */}
     </div>
--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,10 +1,91 @@
 import React from "react";
+import Icon from "@mdi/react";
 import "./post.css";
+import { Avatar } from "@material-ui/core";
+import {
+  mdiCheckDecagram,
+  mdiTwitterRetweet,
+  mdiHeartOutline,
+  mdiHeart,
+  mdiUpload,
+  mdiCommentOutline,
+} from "@mdi/js";
 
-interface Props {}
+interface Props {
+  displayName?: string;
+  username?: string;
+  verified?: boolean;
+  timestamp?: string;
+  text?: string;
+  avatar?: string;
+  image?: string;
+}
 
-const Post = (props: Props) => {
-  return <div></div>;
+const Post: React.FC<Props> = ({
+  displayName,
+  username,
+  verified,
+  timestamp,
+  image,
+  avatar,
+  text,
+}) => {
+  return (
+    <div className="post">
+      <div className="post__avatar">
+        <Avatar src={avatar} />
+      </div>
+      <div className="post__body">
+        <div className="post__header">
+          <div className="post__headerText">
+            <span className="post__displayName">{displayName}</span>
+            {verified && (
+              <span>
+                <Icon
+                  path={mdiCheckDecagram}
+                  title="Verified"
+                  size={0.5}
+                  color="#1da1f2"
+                  style={{ alignSelf: "center" }}
+                />
+              </span>
+            )}
+            <span className="post_headerUsername">{username}</span>
+          </div>
+          <div className="post__headerDescription">
+            BUilding twitter clone awesome
+          </div>
+        </div>
+        <div className="post__image">
+          {image && <img src={image} alt={displayName} />}
+        </div>
+        <div className="post__footer">
+          <Icon
+            path={mdiCommentOutline}
+            title="comment"
+            size={0.7}
+            color="#F2F6FF"
+          />
+
+          <Icon
+            path={mdiTwitterRetweet}
+            title="retweet"
+            size={0.9}
+            color="#F2F6FF"
+          />
+
+          <Icon
+            path={mdiHeartOutline}
+            title="like"
+            size={0.7}
+            color="#F2F6FF"
+          />
+
+          <Icon path={mdiUpload} title="share" size={0.7} color="#F2F6FF" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
